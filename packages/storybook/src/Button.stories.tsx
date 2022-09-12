b3ec0ee0d05403feef1aca961c2bceaf46a64ee7
tkeiyama <thomas18thx@gmail.com>
2022-09-13 00:06:01 +0900

    Add a sample of storybook

---
+++ packages/storybook/src/Button.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Button } from './Button'
+
+export default {
+  title: "storybook/Button",
+  component: Button
+} as ComponentMeta<typeof Button>
+
+export const Preview: ComponentStory<typeof Button> = (props) => {
+  return <Button {...props} />
+}
+
+Preview.args = {
+  children: "Button"
+}
---
+++ packages/storybook/src/Button.tsx
@@ -0,0 +1,10 @@
+import { MouseEventHandler, ReactElement, ReactNode } from "react"
+
+export interface ButtonProps {
+  children: ReactNode
+  onClick: MouseEventHandler<HTMLButtonElement>
+}
+
+export function Button({ children, onClick }: ButtonProps): ReactElement {
+  return <button onClick={onClick}>{children}</button>
+}
