a1dd8180c91a4bbec77e892d02ba926a169ef410
tkeiyama <thomas18thx@gmail.com>
2022-09-12 23:41:29 +0900

    Add files for tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./packages/**/*.ts",
+    "./packages/**/*.tsx",
+    "./.storybook/preview.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
