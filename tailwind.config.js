02a9026394891e010154d9cd36bf1de0f0da9efe
tkeiyama <thomas18thx@gmail.com>
2022-10-05 23:01:41 +0900

    Set directories by using path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+const path = require('path');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
   content: [
-    "./packages/**/*.ts",
-    "./packages/**/*.tsx",
-    "./.storybook/preview.tsx",
+    path.join(__dirname, './packages/**/*.{js,ts,jsx,tsx}'),
+    path.join(__dirname, './.storybook/preview.tsx'),
   ],
   theme: {
     extend: {},
