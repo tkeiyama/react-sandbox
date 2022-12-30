9b665ac4a8ddaaf788ec01a6fbbd15e0f024d306
tkeiyama <thomas18thx@gmail.com>
2022-12-31 05:25:02 +0900

    Bump @thomathings/module-ui-react to v0.2.1

--- packages/thomathings-module-ui/.storybook/main.js
+++ packages/thomathings-module-ui/.storybook/main.js
@@ -1,6 +1,5 @@
 const setConfig = require("../../../.storybook/getStories");
 
 module.exports = {
-  //...setConfig("../src/**/*.stories.tsx"),
-  stories: ["../src/**/*.stories.tsx"]
+  ...setConfig("../src/**/*.stories.tsx"),
 };
---
+++ packages/thomathings-module-ui/src/index.stories.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@thomathings/module-ui-react"
+
+export default {
+  title: "BUTTON"
+}
+
+export const Preview = () => {
+  return <Button>HELLO</Button>
+}
